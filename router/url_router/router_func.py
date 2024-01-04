6e32b5e6d2af6ecb6b43b4c4b372785cdd6257e2
Cleison Amorim <amorim.cleison@gmail.com>
2024-01-04 13:01:03 -0300

    Adjusting script

--- router/url_router/router_func.py
+++ router/url_router/router_func.py
@@ -12,7 +12,7 @@ class RouterFunction():
     def process(self, req: func.HttpRequest) -> func.HttpResponse:
         logging.info("Processing route request...")
         targetUrl = None
-        
+
         # Log request:
         self.__log_request(req)
 
@@ -21,8 +21,9 @@ class RouterFunction():
             originUrl = self.__read_origin_url(req)
 
             if originUrl is None:
-                raise MissingArgumentError(f"Parameter '{ORIGIN_URL_PARAM}' was not provided")
-            
+                raise MissingArgumentError(
+                    f"Parameter '{ORIGIN_URL_PARAM}' was not provided")
+
             # Find target URL:
             logging.info(f"Parameter '{ORIGIN_URL_PARAM}' found: {originUrl}")
             logging.info(f"Processing target route...")
@@ -30,7 +31,8 @@ class RouterFunction():
 
             # Check if a route was found:
             if targetUrl is None:
-                raise RouteNotFoundError("No route URL found for the path informed")
+                raise RouteNotFoundError(
+                    "No route URL found for the path informed")
 
             # Process response:
             response = self.__process_response(targetUrl)
@@ -43,18 +45,17 @@ class RouterFunction():
         self.__log_response(response)
         return response
 
-
     def __parse_to_url(self, originUrl):
         try:
             return urlparse(originUrl)
         except Exception as e:
-            raise InvalidArgumentError(f"Invalid URL provided in the '{ORIGIN_URL_PARAM}' parameter") from e
-
+            raise InvalidArgumentError(
+                f"Invalid URL provided in the '{ORIGIN_URL_PARAM}' parameter") from e
 
     def __read_origin_url(self, req):
         originUrl = None
         read_methods = [self.__read_as_get, self.__read_as_post]
-            
+
         for method_fn in read_methods:
             originUrl = method_fn(req)
 
@@ -62,17 +63,16 @@ class RouterFunction():
                 originUrl = self.__parse_to_url(originUrl)
                 break
         return originUrl
-        
 
     def __read_as_get(self, req):
         logging.info(f"Reading '{ORIGIN_URL_PARAM}' as GET parameter...")
         return req.params.get(ORIGIN_URL_PARAM)
 
-
     def __read_as_post(self, req):
         originUrl = None
-        logging.info(f"Trying to read '{ORIGIN_URL_PARAM}' from POST parameters...")
-        
+        logging.info(
+            f"Trying to read '{ORIGIN_URL_PARAM}' from POST parameters...")
+
         try:
             req_body = req.get_json()
         except ValueError as e:
@@ -81,16 +81,15 @@ class RouterFunction():
             originUrl = req_body.get(ORIGIN_URL_PARAM)
         return originUrl
 
-
     def __process_response(self, targetUrl):
-        return func.HttpResponse(
-            body=json.dumps({ 'url': targetUrl }), 
-            mimetype="application/json", 
-            status_code=302)
-
+        return func.HttpResponse(headers={'Location': targetUrl},
+                                 body=json.dumps({'url': targetUrl}),
+                                 mimetype="application/json",
+                                 status_code=302)
 
     def __process_error_response(self, error):
-        response = None
+        message = None
+        status = 500
 
         if error:
             error_codes_types = {
@@ -102,16 +101,18 @@ class RouterFunction():
             # Find matching code / error type:
             for code, types in error_codes_types.items():
                 if error_type in types:
-                    response = func.HttpResponse(str(error), status_code=400)
+                    message = str(error)
+                    status = 400
 
         # If not matched, returns default error response:
-        if response is None:
-            msg = "Failed to resolve target URL"
-            msg = f"{msg}: {error}" if error else msg
-            response = func.HttpResponse(msg, status_code=500)
-        
-        return response
+        if message is None:
+            message = "Failed to resolve target URL"
+            message = f"{message}: {error}" if error else message
 
+        return func.HttpResponse(
+            body=json.dumps({'error': message}),
+            mimetype="application/json",
+            status_code=status)
 
     def __log_request(self, req: func.HttpRequest):
         request_data = {
@@ -124,7 +125,6 @@ class RouterFunction():
         }
         logging.info(f">>> Request received: {request_data}")
 
-
     def __log_response(self, resp: func.HttpResponse):
         response_data = {
             "status_code": resp.status_code,
@@ -135,17 +135,17 @@ class RouterFunction():
         }
         logging.info(f"<<< Response returned: {response_data}")
 
-
     def __map_to_str(self, m):
-        return [ f"{k}: {v}" for k, v in m.items() ]
-    
+        return [f"{k}: {v}" for k, v in m.items()]
 
 
 class InvalidArgumentError(ValueError):
     pass
 
+
 class MissingArgumentError(ValueError):
     pass
+
 
 class RouteNotFoundError(Exception):
     pass
