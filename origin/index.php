c4cca092b14a592c64de517e919aa1066f017d83
Cleison Amorim <amorim.cleison@gmail.com>
2024-01-04 13:59:52 -0300

    Improving PHP script

--- origin/index.php
+++ origin/index.php
@@ -1,40 +1,22 @@
 <html>
+
 <head>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
 </head>
+
 <body>
-    <?php 
-        $routerUrl = "https://cca5-cin-ufpe-br.azurewebsites.net/api/HttpRouter";
-        $requestUri = $_SERVER['REQUEST_URI'];
-        
-        // Prepare params:
-        $data = $_SERVER;
-        $data['originUrl'] = $requestUri;
+    <?php
+    $routerUrl = "https://cca5-cin-ufpe-br.azurewebsites.net/api/url_router";
+    $params = array(
+        'originUrl' => $_SERVER['REQUEST_URI'],
+    );
+    $targetUrl = $routerUrl . "?" . http_build_query($params);
 
-        // foreach ($data as $parm => $value)  echo "<ul><li>$parm = '$value'</li></ul>";
-
-        // Prepare request:
-        $options = array(
-            'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($data)
-            )
-        );
-        $context  = stream_context_create($options);
-
-        // Send request:
-        $result = file_get_contents($routerUrl, false, $context);
-        
-        // Handle result:
-        if ($result === TRUE) { 
-            echo '<p>Redirecting to the target page...</p>';
-        } else {
-            echo 
-                '<h3>This page is unavailable at the moment</h3>
-                <p>Please, contact me by the email: <a href="mailto:cca5@cin.ufpe.br">cca5@cin.ufpe.br</a></p>';
-        }
+    header($_SERVER["SERVER_PROTOCOL"] . " 301 Moved Permanently");
+    header("Location: " . $targetUrl);
+    exit();
     ?>
 </body>
+
 </html>
